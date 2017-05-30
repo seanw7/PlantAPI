076a6e81c8ab60ca306111ccb912a41e895d14bd
Sean Wilkie <seanw@protonmail.ch>
2017-05-29 20:54:12 -0700

    Made it so all endpoints require proper authentication by a registered user

--- code/resources/genus.py
+++ code/resources/genus.py
@@ -1,13 +1,18 @@
 from flask_restful import Resource
 from models.genus import GenusModel
 
+from flask_jwt import jwt_required
+
 class Genus(Resource):
+
+    @jwt_required()
     def get(self, name):
         genus = GenusModel.find_by_name(name)
         if genus:
             return genus.json()
         return {'message': 'Genus not found'}, 404
 
+    @jwt_required()
     def post(self, name):
         genus = GenusModel.find_by_name(name)
         if genus:
@@ -21,6 +26,7 @@ class Genus(Resource):
 
         return genus.json(), 201
 
+    @jwt_required()
     def delete(self, name):
         genus = GenusModel.find_by_name(name)
         if genus:
@@ -30,5 +36,7 @@ class Genus(Resource):
 
 
 class GenusList(Resource):
+    
+    @jwt_required()
     def get(self):
         return {'genera': [genus.json() for genus in GenusModel.query.all()]}
